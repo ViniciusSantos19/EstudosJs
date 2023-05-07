6b2b51d2be0d0588f3bb306a92226fd1812da47c
ViniciusSantos19 <vinicontatosq@gmail.com>
2023-05-07 14:53:54 -0300

    cap4

---
+++ Capitulo4/Capitulo4/client/app/controllers/NegociacaoController.js
@@ -0,0 +1,30 @@
+class NegociacaoController {
+    
+    constructor () {
+
+        let $ = document.querySelector.bind(document);
+
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+
+    }
+    
+    adicionar(event){
+        event.preventDefault();
+
+
+        let data = DateConverter.paraData(this._inputData.value);
+
+        let negociacao = new Negociacao(data, 
+            parseInt(this._inputQuantidade.value), 
+            parseFloat(this._inputValor.value));
+
+                            
+        let diaMesAno = DateConverter.paraTexto(negociacao.data);
+                            
+        console.log(negociacao.quantidade);
+        console.log(diaMesAno);
+        
+    }
+}
