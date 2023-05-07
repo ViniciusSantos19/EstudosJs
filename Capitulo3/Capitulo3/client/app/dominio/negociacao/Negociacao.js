445640793659caa82ec7924e250062f7da7bfc5f
ViniciusSantos19 <vinicontatosq@gmail.com>
2023-05-07 13:30:06 -0300

    first commit

---
+++ Capitulo3/Capitulo3/client/app/dominio/negociacao/Negociacao.js
@@ -0,0 +1,40 @@
+class Negociacao{
+
+   
+
+    constructor(_data, _quantidade, _valor) {
+
+        Object.assign(this, {_quantidade , _valor});
+        this._data = new Date(_data.getTime());
+        Object.freeze(this);
+    }
+
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+
+    get data() {
+        return new Date(this._data.getTime());
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor() {
+        return this._valor;
+    }
+
+    setData(data) {
+        this._data = data;
+    }
+
+    setQuantidade(quantidade) {
+        this._quantiade = quantidade;
+    }
+
+    setValor(valor) {
+        this._valor = valor;
+    }
+
+}
